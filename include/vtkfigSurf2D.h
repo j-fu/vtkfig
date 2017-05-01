378c6c43eed2dfd19b317b18127effcb30b1cdc7
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2017-05-01 20:49:28 +0200

    extra class for vtkSurf2D

---
+++ include/vtkfigSurf2D.h
@@ -0,0 +1,97 @@
+#ifndef VTKFIG_SURF2D_H
+#define VTKFIG_SURF2D_H
+
+#include "vtkRectilinearGrid.h"
+#include "vtkStructuredGrid.h"
+#include "vtkPointData.h"
+#include "vtkFloatArray.h"
+
+
+#include "vtkfigFigure.h"
+
+namespace vtkfig
+{
+
+  ///////////////////////////////////////////
+  class Surf2D: public Figure
+    {
+    public:
+      
+      Surf2D();
+      
+
+      template<typename V>
+      void Add(const V &xcoord, 
+               const V &ycoord, 
+               const V &values);
+
+      void SetRGBTable(RGBTable & tab, int tabsize)
+      {
+        lut=BuildLookupTable(tab,tabsize);
+      }
+      void ShowColorbar(bool b) {show_colorbar=b;}
+
+    private:
+      void Add(vtkSmartPointer<vtkStructuredGrid> gridfunc, double Lxy, double Lz);
+      vtkSmartPointer<vtkLookupTable> lut;
+      bool show_colorbar=true;
+  };
+
+
+  template<typename V>
+  inline
+  void Surf2D::Add(const V &x, const V &y, const V &z)
+  {
+    const unsigned int Nx = x.size();
+    const unsigned int Ny = y.size();
+    int i,j,k;
+    double Lxy,Lz;
+    
+    if (x[Nx-1]-x[0] > y[Ny-1]-y[0])
+      Lxy = x[Nx-1]-x[0];
+    else
+      Lxy = y[Ny-1]-y[0];
+    double z_low = 10000, z_upp = -10000;
+    
+    
+    vtkSmartPointer<vtkStructuredGrid> 	    gridfunc= vtkSmartPointer<vtkStructuredGrid>::New();
+    gridfunc->SetDimensions(Nx, Ny, 1);
+    
+    vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+    for (j = 0; j < Ny; j++)
+    {
+      for (i = 0; i < Nx; i++)
+      {
+        points->InsertNextPoint(x[i], y[j], z[j*Nx+i]);
+        
+        if (z[j*Nx+i]< z_low)
+          z_low = z[j*Nx+i];
+        if (z[j*Nx+i]> z_upp)
+          z_upp = z[j*Nx+i];
+      }
+    }
+    gridfunc->SetPoints(points);
+    
+    
+    vtkSmartPointer<vtkFloatArray> colors = vtkSmartPointer<vtkFloatArray>::New();
+    colors->SetNumberOfComponents(1);
+    colors->SetNumberOfTuples(Nx*Ny);
+    k = 0;
+    for (j = 0; j < Ny; j++)
+      for (i = 0; i < Nx; i++)
+      {
+        colors->InsertComponent(k, 0, z[j*Nx+i]);
+        k++;
+      }
+    
+    gridfunc->GetPointData()->SetScalars(colors);
+    
+    Lz = z_upp-z_low;
+    
+    Add(gridfunc,Lxy,Lz);
+    
+  }
+
+}
+
+#endif
---
+++ src/vtkfigSurf2D.cxx
@@ -0,0 +1,103 @@
+#include "vtkActor.h"
+#include "vtkProperty.h"
+#include "vtkTextProperty.h"
+#include "vtkOutlineFilter.h"
+#include "vtkStructuredGridGeometryFilter.h"
+#include "vtkWarpScalar.h"
+#include "vtkAxesActor.h"
+#include "vtkCaptionActor2D.h"
+
+#include "vtkfigSurf2D.h"
+
+namespace vtkfig
+{
+////////////////////////////////////////////////////////////
+  Surf2D::Surf2D(): Figure()
+  {
+    RGBTable surface_rgb={{0,0,0,1},{1,1,0,0}};
+    lut=BuildLookupTable(surface_rgb,255);
+  }
+
+
+  void Surf2D::Add(vtkSmartPointer<vtkStructuredGrid> gridfunc, double Lxy, double Lz)
+  {
+
+    bool do_warp=true;
+    bool draw_box=true;
+    bool draw_axes=true;
+    bool draw_colorbar=true;
+   
+    // filter to geometry primitive
+    vtkSmartPointer<vtkStructuredGridGeometryFilter>geometry =
+      vtkSmartPointer<vtkStructuredGridGeometryFilter>::New();
+    geometry->SetInputDataObject(gridfunc);
+
+    // warp to fit in box
+    vtkSmartPointer<vtkWarpScalar> warp = vtkSmartPointer<vtkWarpScalar>::New();
+    if (do_warp)
+    {
+	double scale = Lxy/Lz;
+	warp->SetInputConnection(geometry->GetOutputPort());
+	warp->XYPlaneOn();
+	warp->SetScaleFactor(scale);
+    }
+
+    // map gridfunction
+    vtkSmartPointer<vtkPolyDataMapper> mapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    if (do_warp)
+	mapper->SetInputConnection(warp->GetOutputPort());
+    else
+	mapper->SetInputConnection(geometry->GetOutputPort());
+
+    double vrange[2];
+    gridfunc->GetScalarRange(vrange);
+    mapper->SetScalarRange(vrange[0], vrange[1]);
+
+    // create plot surface actor
+    vtkSmartPointer<vtkActor> surfplot = vtkSmartPointer<vtkActor>::New();
+    surfplot->SetMapper(mapper);
+    mapper->SetLookupTable(lut);
+
+    // create outline
+    vtkSmartPointer<vtkOutlineFilter> outlinefilter = vtkSmartPointer<vtkOutlineFilter>::New();
+    if (do_warp)
+	outlinefilter->SetInputConnection(warp->GetOutputPort());
+    else
+	outlinefilter->SetInputConnection(geometry->GetOutputPort());
+
+    vtkSmartPointer<vtkPolyDataMapper>outlineMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+    outlineMapper->SetInputConnection(outlinefilter->GetOutputPort());
+    vtkSmartPointer<vtkActor> outline = vtkSmartPointer<vtkActor>::New();
+    outline->SetMapper(outlineMapper);
+    outline->GetProperty()->SetColor(0, 0, 0);
+
+    // create axes
+    vtkSmartPointer<vtkAxesActor> axes = vtkSmartPointer<vtkAxesActor>::New();
+    axes->SetShaftTypeToCylinder();
+    axes->SetNormalizedShaftLength( 0.85, 0.85, 0.85);
+    axes->SetNormalizedTipLength( 0.15, 0.15, 0.15);
+    axes->SetCylinderRadius( 0.500 * axes->GetCylinderRadius() );
+    axes->SetConeRadius( 1.025 * axes->GetConeRadius() );
+    axes->SetSphereRadius( 1.500 * axes->GetSphereRadius() );
+    vtkSmartPointer<vtkTextProperty> text_prop_ax = axes->GetXAxisCaptionActor2D()->
+	GetCaptionTextProperty();
+    text_prop_ax->SetColor(0.0, 0.0, 0.0);
+    text_prop_ax->SetFontFamilyToArial();
+    text_prop_ax->SetFontSize(8);
+    axes->GetYAxisCaptionActor2D()->SetCaptionTextProperty(text_prop_ax);
+    axes->GetZAxisCaptionActor2D()->SetCaptionTextProperty(text_prop_ax);
+
+
+
+    // renderer
+    Figure::AddActor(surfplot);
+    if (draw_box)
+	Figure::AddActor(outline);
+    if (draw_axes)
+	Figure::AddActor(axes);
+    if (show_colorbar)
+      Figure::AddActor(Figure::BuildColorBar(mapper));
+    
+  }
+
+}
