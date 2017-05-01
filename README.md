c16ed14ec686813644458db4d201ca508a449694
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2017-05-01 19:41:33 +0200

    Code reorganization

---
+++ README.md
@@ -0,0 +1,8 @@
+visvtk               started                from               matplot
+(http://www.csc.kth.se/~dag/matplot_20091021.tar.gz,     found     via
+http://na-wiki.csc.kth.se/mediawiki/index.php/Mat_plot_vtk)   by   Dag
+Lindbo.
+
+changes:
+- vtk7  port
+- depdendency soley on std::vector
---
+++ examples/example-contour3d.cxx
@@ -0,0 +1,96 @@
+#include "vtkfig.h"
+
+
+inline double G(double x,double y, double z, double t) 
+{
+  
+  return exp(-(x*x+y*y+z*z))*sin(t+x)*cos(y-t)*sin(0.5*z-0.5*t);
+}
+
+
+int main(void)
+{
+  
+  const int Nx = 40;
+  const int Ny = 40;
+  const int Nz = 40;
+  
+  std::vector<double> x(Nx);
+  std::vector<double> y(Ny);
+  std::vector<double> z(Nz);
+
+  std::vector<double> v(Nx*Ny*Nz);
+
+  
+  const double x_low = -2.5;
+  const double x_upp = 1.5;
+
+  const double y_low = -2.5;
+  const double y_upp = 4;
+
+  const double z_low = -2.5;
+  const double z_upp = 4;
+
+  const double dx = (x_upp-x_low)/(Nx-1);
+  const double dy = (y_upp-y_low)/(Ny-1);
+  const double dz = (z_upp-z_low)/(Nz-1);
+
+  auto frame=vtkfig::Frame();
+  frame.SetInteractorStyle(vtkfig::Frame::InteractorStyle::Volumetric);
+  auto colors=vtkfig::Figure::RGBTable
+    { 
+      {0.0, 0.0, 0.0, 1.0},
+      {0.5, 0.0, 1.0, 0.0},
+      {1.0, 1.0, 0.0, 0.0}
+    };
+
+  for (int i=0; i<Nx; i++)
+    x[i] = x_low+i*dx;
+  
+  for (int i=0; i<Ny; i++)
+    y[i] = y_low + i*dy;
+
+  for (int i=0; i<Nz; i++)
+    z[i] = z_low + i*dz;
+
+
+  double t=0;
+  double dt=0.01;
+  int ii=0;
+  double t0=(double)clock()/(double)CLOCKS_PER_SEC;
+  double i0=ii;
+  while (1)
+  {
+    auto contour=vtkfig::Contour3D();
+    contour.SetContourRGBTable(colors,255);
+
+    for (int i=0; i<Nx; i++)
+      for (int j=0; j<Ny; j++)
+        for (int k=0; k<Nz; k++)
+      {
+        v[k*Nx*Ny+j*Nx+i] = G(x[i],y[j],z[k],t);
+      }
+
+    assert(v.size()==(Nx*Ny*Nz));
+
+    contour.Add(x,y,z,v);
+    frame.Clear();
+    frame.Add(contour);
+    frame.Show();
+
+    if (ii==3) 
+      frame.Dump("example-contour3d.png");
+
+    t+=dt;
+    double t1=(double)clock()/(double)CLOCKS_PER_SEC;
+    double i1=ii;
+    if (t1-t0>4.0)
+    {
+      printf("Frame rate: %.2f fps\n",(double)(i1-i0)/4.0);
+      t0=(double)clock()/(double)CLOCKS_PER_SEC;
+      i0=ii;
+    }
+    ii++;
+  }
+
+}
